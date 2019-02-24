4e88d80ab2b45cdfb9780df620b93f9f7e74e2f4
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-02-24 15:37:23 +0800

    restructured the project (mainly the command-line parsing)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const request = require('request');
-const args = require('args-parser')(process.argv);
+const program = require('commander');
+const colors = require('colors');
 
-if (args["get-user"]) {
-  if (args["u"] || args["user"]) {
-    handle = args["u"] || args["user"];
+
+function getUser(handle) {
     request('http://codeforces.com/api/user.info?handles=' + handle, function (error, response, body) {
         if (error) {
             console.error(error);
@@ -15,7 +15,7 @@ if (args["get-user"]) {
                 console.log(result.comment);
             } else {
                 result = result.result[0];
-                console.log('[-] Handle: ' + handle);
+                console.log('[-] Handle: ' + result.handle);
                 console.log('[-] Name: ' + result.firstName + ' ' + result.lastName);
                 console.log('[-] Rank: ' + result.rank);
                 console.log('[-] Best Rank: ' + result.maxRank);
@@ -28,32 +28,133 @@ if (args["get-user"]) {
             }
         }
     });
-  }
-} else if (args["con-stat"]) {
-// TODO:
-
-
-} else if (args["gen-problem"]) {
-// TODO:
-
-
-} else if (args["rec-act"]) {
-// TODO:
-
-
-} else if (args["usr-submission"]) {
-// TODO:
-
-
-} else if (args["sub-code"]) {
-// TODO:
-
-
-} else if (args["com-contest"]) {
-// TODO:
-
-
-} else{
-  // TODO:
-  console.log("Invalid Function");
 }
+
+
+function submissionCode(submissionId) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+function contestStatistics(handle) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+function generateProblems(handle, count, tag, difficulty) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+function recentActions(count) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+function userSubmissions(handle, count) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+function upcomingContests(count) {
+    // TODO:
+    console.log(colors.green("coming Soon :)"));
+}
+
+
+
+program
+    .version('0.0.1', '-v, --version');
+
+program.on('--help', () => {
+    console.log('  All options:');
+    console.log('    -u, --user                handle of the required user');
+});
+
+
+program
+    .command('get-user')
+    .option('-u, --user', 'handle of the required user')
+    .action(function (usr) {
+        if (typeof usr === "string")
+            getUser(usr);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+program
+    .command('sub-code')
+    .option('-s, --submission', 'submission id required')
+    .action(function (id) {
+        if (typeof id === "string")
+            submissionCode(id);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+
+program
+    .command('con-stat')
+    .option('-u, --user', 'handle of the required user')
+    .action(function (usr) {
+        if (typeof usr === "string")
+            contestStatistics(usr);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+
+program
+    .command('gen-problems')
+    .option('-u, --user', 'handle of the required user')
+    .option('-c, --count', 'number of problems to be generated')
+    .option('-t, --tag', 'category of the problem to be generated')
+    .option('-d, --difficulty', 'difficulty of the required problems')
+    .action(function (usr, cnt, tag, dif) {
+        if (typeof usr === "string" && typeof cnt === "string" &&
+            typeof tap === "string" && typeof dif === "string")
+            generateProblems(usr, cnt, tag, dif);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+
+program
+    .command('rec-action')
+    .option('-c, --count', 'number of blogs to get')
+    .action(function (cnt) {
+        if (typeof cnt === "string")
+            recentActions(cnt);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+
+program
+    .command('com-contest')
+    .option('-c, --count', 'max number of upcoming contest to print')
+    .action(function (cnt) {
+        if (typeof cnt === "string")
+            upcomingContests(cnt);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+
+program
+    .command('usr-submission')
+    .option('-u, --user', 'handle of the required user')
+    .option('-c, --count', 'number of problems to be generated')
+    .action(function (usr, cnt) {
+        if (typeof usr === "string" && typeof cnt === "string")
+            userSubmissions(usr, cnt);
+        else
+            console.log("Invalid!!!!!!");
+    });
+
+program.parse(process.argv);
